a9588e5295a8f446ca17c81b6b38a13fb653b66c
David Nicholson <NickleDave@users.noreply.github.com>
2023-11-12 16:57:08 -0500

    Finish adding setup scripts etc (#2)
    * Update README with instructions
    
    * Add noxfile.py
    
    * Add more to README.md
    
    * Start adding setup session and rewrite download session in noxfile.py
    
    * Add/pin dependencies in pyproject.toml
    
    * Fix install of vak in nox 'setup' session
    
    * Add data/prep/multiclass/BFSongRepo/.gitkeep
    
    * Fix session name in noxfile

--- README.md
+++ README.md
@@ -4,10 +4,79 @@
 
 ## Set-up
 
+### Pre-requisites
+
+You will need:
+
+1. git for version control
+
+(you can install git from [Github](https://help.github.com/en/github/getting-started-with-github/set-up-git),
+with your operating system package manager, or using conda.)
+
+2. nox for running tasks
+
+This project uses the library [nox](https://nox.thea.codes/en/stable/)
+as a [task runner](https://scikit-hep.org/developer/tasks),
+to automate tasks like setting up a virtual environment.
+Each task is represented as what nox calls a "session",
+and you can run a session by invoking nox
+at the command-line with the name of the session, 
+e.g. `nox -s setup` will run the session called "setup". 
+We suggest using [pipx](https://github.com/pypa/pipx)
+to install nox in its own isolated environment,
+so that nox can be accessed system-wide without affecting
+anything else on your machine.
+
+To install nox this way:
+
+  1. Install pipx, e.g. with the package manager [brew](https://github.com/pypa/pipx#on-macos)
+(note that [brew works on Linux too](https://docs.brew.sh/Homebrew-on-Linux)).
+
+  2. Install nox with pipx: `pipx install nox`
+
+For other ways to install nox, please see:
+https://nox.thea.codes/en/stable/tutorial.html#installation
+
 ### Set up environment and install code
 
+Experiments were run on [Pop!_OS 22.04](https://pop.system76.com/).  
+It will be easiest to set up in a similar Linux environment (e.g., Ubuntu).
 
-### Download data + results
+1. Clone this repository with git:
 
-## Run results
+```
+git clone git@github.com:vocalpy/Nicholson-Cohen-SfN-2023-poster.git
+cd Nicholson-Cohen-SfN-2023-poster
+```
 
+2. Set up the virtual environment with the code installed into it, by running the nox session called "setup":
+
+```nox -s setup```
+
+### Download dataset + results
+
+3. To test whether you can replicate experiments and analysis, you'll need to download the dataset and the results
+
+Run the nox session called "download":  
+
+```nox -s download```
+
+- The dataset can be found here on Zenodo: 
+  https://zenodo.org/records/10098250
+
+- The results can be in this Open Science Framework project:  
+  https://osf.io/3yv8s/
+
+## Usage
+
+### Re-run experiments
+
+To re-run any individual configuration file, run this command in the activated virtual environment:
+
+```
+python src/scripts/learncurve.py learncurve data/configs/NAME-OF-CONFIG.toml
+```
+
+We provide bash scripts to re-run the experiments testing the effect of window size and of 
+
+### Re-run analysis
---
+++ noxfile.py
@@ -0,0 +1,179 @@
+import dataclasses
+import os
+import pathlib
+import tarfile
+import urllib.request
+
+import nox
+
+
+DIR = pathlib.Path(__file__).parent.resolve()
+VENV_DIR = pathlib.Path('./.venv').resolve()
+
+
+@nox.session(python="3.10.7")
+def setup(session: nox.Session) -> None:
+    """
+    Sets up a python environment for the project.
+
+    This session will:
+    - Create a python virtualenv for the session
+    - Install the `virtualenv` cli tool into this environment
+    - Use `virtualenv` to create a global project virtual environment
+    - Invoke the python interpreter from the global project environment to install
+      the project and all its development dependencies.
+    """
+    session.install("virtualenv")
+    # VENV_DIR here is a pathlib.Path location of the project virtualenv
+    # e.g. .venv
+    session.run("virtualenv", os.fsdecode(VENV_DIR), silent=True)
+
+    # Use the venv's interpreter to install the project along with
+    # all it's dev dependencies, this ensures it's installed in the right way
+    python = os.fsdecode(VENV_DIR.joinpath("bin/python"))
+
+    # NOTE we install vak with the specific commit used to run experiments
+    session.run(python, "-m", "pip", "install", "vak @ git+https://github.com/vocalpy/vak@e73f6a396770870c303c1c3574071b53971fc16c", external=True)
+    # then we install everything else
+    session.run(python, "-m", "pip", "install", "-e", ".", external=True)
+
+
+@dataclasses.dataclass
+class DownloadItem:
+    """Dataclass representing item to download:
+    either the dataset on Zenodo, or results on OSF"""
+    name: str
+    url: str
+    path: str
+
+
+TO_DOWNLOAD = [
+    # ---- dataset
+    DownloadItem(
+        name="dataset",
+        url="https://zenodo.org/records/10098250/files/all-birds-vak-frame-classification-dataset-generated-231025_173401.tar.gz",
+        path="data/prep/multiclass/BFSongRepo/all-birds-vak-frame-classification-dataset-generated-231025_173401.tar.gz",
+    ),
+    # ---- results
+    DownloadItem(
+        name="result",
+        url="https://osf.io/jnd9x/download",
+        path="results/multiclass/results_231025_200608.tar.gz",
+    ),
+    DownloadItem(
+        name="result",
+        url="https://osf.io/c6m2a/download",
+        path="results/multiclass/results_231025_200702.tar.gz",
+    ),
+    DownloadItem(
+        name="result",
+        url="https://osf.io/snmfk/download",
+        path="results/multiclass/results_231025_222720.tar.gz",
+    ),
+    DownloadItem(
+        name="result",
+        url="https://osf.io/qxndm/download",
+        path="results/multiclass/results_231025_222812.tar.gz",
+    ),
+    DownloadItem(
+        name="result",
+        url="https://osf.io/xm4ps/download",
+        path="results/multiclass/ results_231026_085351.tar.gz",
+    ),
+    DownloadItem(
+        name="result",
+        url="https://osf.io/j7h2f/download",
+        path="results/multiclass/results_231026_103215.tar.gz",
+    ),
+    DownloadItem(
+        name="result",
+        url="https://osf.io/pmc2u/download",
+        path="results/multiclass/results_231027_162429.tar.gz",
+    ),
+    DownloadItem(
+        name="result",
+        url="https://osf.io/sp927/download",
+        path="results/multiclass/results_231027_162459.tar.gz",
+    ),
+    DownloadItem(
+        name="result",
+        url="https://osf.io/mryq5/download",
+        path="results/multiclass/results_231028_080948.tar.gz",
+    ),
+    DownloadItem(
+        name="result",
+        url="https://osf.io/urdm8/download",
+        path="results/multiclass/results_231028_124122.tar.gz",
+    ),
+    DownloadItem(
+        name="result",
+        url="https://osf.io/xd3y2/download",
+        path="results/multiclass/results_231103_180218.tar.gz",
+    ),
+    DownloadItem(
+        name="result",
+        url="https://osf.io/y35j7/download",
+        path="results/multiclass/results_231103_180825.tar.gz",
+    ),
+    DownloadItem(
+        name="result",
+        url="https://osf.io/rt6ej/download",
+        path="results/multiclass/results_231104_064536.tar.gz",
+    ),
+    DownloadItem(
+        name="result",
+        url="https://osf.io/ph7qu/download",
+        path="results/multiclass/ results_231104_092453.tar.gz",
+    ),
+    DownloadItem(
+        name="result",
+        url="https://osf.io/n5ay7/download",
+        path="results/multiclass/results_231104_114724.tar.gz",
+    ),
+    DownloadItem(
+        name="result",
+        url="https://osf.io/br4n5/download",
+        path="results/multiclass/results_231025_200608.tar.gz",
+    ),
+    DownloadItem(
+        name="result",
+        url="https://osf.io/j86bz/download",
+        path="results/multiclass/results_231104_152144.tar.gz",
+    ),
+    DownloadItem(
+        name="result",
+        url="https://osf.io/jnd9x/download",
+        path="results/multiclass/results_231104_194434.tar.gz",
+    ),
+    DownloadItem(
+        name="result",
+        url="https://osf.io/uafmj/download",
+        path="results/multiclass/ results_231104_215302.tar.gz",
+    ),
+    DownloadItem(
+        name="result",
+        url="https://osf.io/xgujh/download",
+        path="results/multiclass/results_231105_175621.tar.gz",
+    ),
+]
+
+
+def copy_url(url: str, path: str) -> None:
+    """Copy data from a url to a local file."""
+    urllib.request.urlretrieve(url, path)
+
+
+@nox.session
+def download(session) -> None:
+    """
+    Download and extract the dataset and the results.
+    """
+    session.log(f'Downloading dataset and results')
+    for download_item in TO_DOWNLOAD:
+        session.log(
+            f"Downloading {download_item.name} from {download_item.url} to {download_item.path}"
+        )
+        copy_url(url=download_item.url, path=download_item.path)
+        session.log(f'Extracting downloaded tar: {download_item.path}')
+        with tarfile.open(download_item.path, "r:gz") as tf:
+            tf.extractall(path='.')
